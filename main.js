da7727706a552cd30335fb081ff127f3662e5e83
Belokan <Belokan@MacBook-Air-Belokan.local>
2022-10-27 22:09:52 +0200

    ngepet template landing

---
+++ main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(".comment").slick({
+        arrows: true,
+        dots: true,
+        infinite: false,
+        prevArrow: $('.s-arrow-previous'),
+        nextArrow: $('.s-arrow-next')
+    });
+
+});
